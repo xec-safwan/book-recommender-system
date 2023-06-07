388c1097406eea1fd1d362208ccbce51b860553b
Safwan Shaikh <xec.safwan@gmail.com>
2023-06-07 23:53:35 +0530

    Resolved app crashing bug

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import pickle
+import pandas as pd
 
-popular_df = pickle.load(open('popular.pkl','rb'))
+
+
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():  # put application's code here
+    filename = 'popular.pkl'
+    popular_df = pickle.load(open(filename, 'rb'))
     return render_template('index.html',
                            book_name=list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
+                           votes=list(popular_df['num_rating'].values),
                            rating=list(popular_df['avg_rating'].values)
                            )
 
